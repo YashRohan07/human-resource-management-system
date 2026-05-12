a3009350b6551eb910cf846b8e25a424309620fc
Rohan <yashrohan22@gmail.com>
2026-05-12 20:09:31 +0600

    Add JWT auth interceptor

---
+++ hrms-client/src/app/core/interceptors/auth.interceptor.ts
@@ -0,0 +1,23 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+
+import { AuthService } from '../services/auth.service';
+
+// Adds JWT token to outgoing API requests
+export const authInterceptor: HttpInterceptorFn = (request, next) => {
+
+  const authService = inject(AuthService);
+  const token = authService.getToken();
+
+  if (!token) {
+    return next(request);
+  }
+
+  const authRequest = request.clone({
+    setHeaders: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+
+  return next(authRequest);
+};
